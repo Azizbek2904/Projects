b502340168f22371f078415ac810a4882e3f7a16
User <azizbekqudratov537@gmail.com>
2024-02-21 10:23:10 +0500

    regex davomi

--- src/regex/regex.java
+++ src/regex/regex.java
@@ -1,6 +1,17 @@
 package regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class regex {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sana oy yilni kriting: (DD:MM:YYYY) ");
+        String text=  scanner.nextLine();
+        String regex ="^(0[1-9]|1[0-2]):(0[1-9]|[12][0-9]|3[01]):\\\\d{4}$\"";
+        Pattern pattern =Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
 
     }
 }
